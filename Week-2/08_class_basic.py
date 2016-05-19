ef921affcc871efd4523b41119ef63c58fc54c17
LeoHeo <hjh5488@gmail.com>
2016-05-19 20:45:43 +0800

    2주차 내용 정리
    내용 업데이트
    
    추후 Decorator내용 보강

--- Week-2/08_class_basic.py
+++ Week-2/08_class_basic.py
@@ -40,5 +40,64 @@ jinhan = Student("허진한", 28)
 jinhan.campus = "경쟁사"
 jinhan.introduce()
 
+# jinhan Object안에 있는 함수나 변수보기
 dir(jinhan)
 
+
+class Rectangle():
+    #width = ""
+    #height = ""
+    
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def area(self): # 면적
+        return self.width * self.height
+    
+    def girth(self): # 둘레
+        return (self.width + self.height) * 2
+    
+    def is_bigger(self, another):
+        return self.area() > another.area()
+
+rec1 = Rectangle(10, 10)
+rec2 = Rectangle(20, 30)
+
+print( rec1.area())
+print( rec1.girth())
+
+rec1.is_bigger(rec2)
+rec2.is_bigger(rec1)
+
+
+# information class만들고
+# csv파일 열어서 information List 만들기
+
+class Information():
+ 
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        
+    def print_introduce(self):
+        print("{name}님 주소는 {address}입니다".format(
+            name = self.name,
+            address = self.address
+        ))   
+
+
+with open("../../users.csv", "r") as f:    
+    # f.readlines()는 파일 오픈당 한번만 유효함
+    # 그래서 위에 주석처리한거나 아래 List Comprehension 둘중에 하나만 써야함
+    information_list = [
+        Information(
+            user.split(",")[0],
+            user.split(",")[1].replace("\n", "")
+        )
+        for user
+        in f.readlines()
+    ]
+    
+    for info in information_list:
+        info.print_introduce()
---
+++ Week-2/10_Decorator.py
@@ -0,0 +1,3 @@
+## Decorator - function, class
+# 장점: decorator도 의미있는 데이터와, 메소드들로 묶을 수 있다. ( "모듈화" 할 수 있다.)
+
